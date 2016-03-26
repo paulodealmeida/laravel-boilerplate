9a37dd12ca2041642723a17ae99322599a76e57c
Paulo de Almeida <paulodealmeida@gmail.com>
2016-03-26 17:03:46 -0300

    commit inicial

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,15 @@
+<section class="sidebar">
+    <ul class="sidebar-menu">
+        <li class="header"></li>
+        <li>
+            <a href="{{ URL::to('/') }}">
+                <span>Painel</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ URL::to('/route') }}">
+                <span>name</span>
+            </a>
+        </li>
+    </ul>
+</section>
